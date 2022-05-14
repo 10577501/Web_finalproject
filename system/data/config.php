b4f913da8cdfdc3a719a56664655d5a0ebae40d3
Sinyzh <102450906+10577501@users.noreply.github.com>
2022-05-14 18:06:08 +0800

    Add files via upload

---
+++ system/data/config.php
@@ -0,0 +1,13 @@
+<?php
+//数据库配置
+$config = array(
+    'db' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'wwi',
+    'charset' => 'utf8',
+    'user' => 'root',
+    'psd' => 'root',
+);
+return $config;
+
